23bdd0d229f4caa33d1933c0fa130b59346f340b
NasosBoras <61327520+NasosBoras@users.noreply.github.com>
2021-07-23 21:28:04 +0300

    typo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`API on http://localhost:${PORT}`));
 
-app.get("/", (res, req) => {
+app.get("/", (req, res) => {
     res.send("Hello World");
 });
